Fix calculation of sum for even number of unique values in num_vars

streamlit_app.py
@@ -79,12 +79,12 @@ if uploaded_file is not None:
         num_vars_ratio = num_vars_ratio.loc[:, ::-1]
         
         # num_varsの中身のユニークな値の数が偶数だった場合の処理
-        if len(num_vars) % 2 == 0:                     
+        # if len(num_vars) % 2 == 0:                     
             
-            # 列の左半分の合計値を計算
-            num_vars_ratio['肯定群'] = num_vars_ratio.iloc[:, :len(num_vars) // 2].sum(axis=1)
-            # 列の右半分の合計値を計算
-            num_vars_ratio['否定群'] = num_vars_ratio.iloc[:, len(num_vars) // 2:].sum(axis=1)
+        # 列の左半分の合計値を計算
+        num_vars_ratio['肯定群'] = num_vars_ratio.iloc[:, :len(num_vars) // 2].sum(axis=1)
+        # 列の右半分の合計値を計算
+        num_vars_ratio['否定群'] = num_vars_ratio.iloc[:, len(num_vars) // 2:].sum(axis=1)
         
         # 平均値を追加
         num_vars_ratio['平均値'] = df[num_vars].mean()