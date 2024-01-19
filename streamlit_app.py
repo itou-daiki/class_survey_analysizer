909e9efe6c6dcf7f4b09a58a748a94b4987e7c5b
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2024-01-19 13:05:47 +0000

    Refactor code to calculate sum of left and right halves of num_vars_ratio when the number of unique values in num_vars is even.

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,16 +79,12 @@ if uploaded_file is not None:
         num_vars_ratio = num_vars_ratio.loc[:, ::-1]
         
         # num_varsの中身のユニークな値の数が偶数だった場合の処理
-        if len(num_vars) % 2 == 0:
+        if len(num_vars) % 2 == 0:                     
+            
             # 列の左半分の合計値を計算
             num_vars_ratio['肯定群'] = num_vars_ratio.iloc[:, :len(num_vars) // 2].sum(axis=1)
             # 列の右半分の合計値を計算
             num_vars_ratio['否定群'] = num_vars_ratio.iloc[:, len(num_vars) // 2:].sum(axis=1)
-            
-            # 新しい列として追加
-            
-            
-            
         
         # num_vars_ratioを表示
         st.write(num_vars_ratio)        
